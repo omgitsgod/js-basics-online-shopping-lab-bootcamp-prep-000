f63707d90b633b0b510fc81a9ad03148b08301e8
Ryan Travis <rpenchenski@gmail.com>
2018-09-06 20:55:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,6 +74,6 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    return `Your total cost is ${total()}`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   }
 }
